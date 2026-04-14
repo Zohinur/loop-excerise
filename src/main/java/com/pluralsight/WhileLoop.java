269664ab3e0b33737d4393dac84f1fd9d369adbf
zohinurmiah <zohinur.m17@gmail.com>
2026-04-14 13:11:31 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        int j =5;
+        while(i<j) {
+            System.out.println("I love Java");
+            i+=1;
+        }
+
+    }
+}
